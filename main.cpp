6ba046900895346df83237565d201974e9e0b2d9
Souleimane SEGHIR <soulei.seg@gmail.com>
2019-03-21 15:33:52 +0100

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -5,23 +5,23 @@
 #include "MQTTClient.h"
 
 // Network interface
-NetworkInterface net;
+NetworkInterface *net;
 
 int arrivedcount = 0;
-const char topic = "/YNOV/messages";
+const char* topic1 = "LeCoqGalmotSeghir/feeds/projectiotfeeds.tempresult";
 
-/* Printf the message received and its configuration /
+/* Printf the message received and its configuration */
 void messageArrived(MQTT::MessageData& md)
 {
     MQTT::Message &message = md.message;
     printf("Message arrived: qos %d, retained %d, dup %d, packetid %d\r\n", message.qos, message.retained, message.dup, message.id);
-    printf("Payload %.s\r\n", message.payloadlen, (char*)message.payload);
+    printf("Payload %.*s\r\n", message.payloadlen, (char*)message.payload);
     ++arrivedcount;
 }
 
 // MQTT demo
 int main() {
-    int result;
+	int result;
 
     // Add the border router DNS to the DNS table
     nsapi_addr_t new_dns = {
@@ -35,7 +35,7 @@ int main() {
     // Get default Network interface (6LowPAN)
     net = NetworkInterface::get_default_instance();
     if (!net) {
-        printf("Error! No network inteface found.\n");
+        printf("Error! No network interface found.\n");
         return 0;
     }
 
@@ -45,25 +45,40 @@ int main() {
         printf("Error! net->connect() returned: %d\n", result);
         return result;
     }
-// Build the socket that will be used for MQTT
+
+    // Build the socket that will be used for MQTT
     MQTTNetwork mqttNetwork(net);
 
     // Declare a MQTT Client
     MQTT::Client<MQTTNetwork, Countdown> client(mqttNetwork);
 
     // Connect the socket to the MQTT Broker
-    const char* hostname = "fd9f:590a:b158:ffff:ffff::c0a8:0103";
+    const char* hostname = "io.adafruit.com";
     uint16_t port = 1883;
     printf("Connecting to %s:%d\r\n", hostname, port);
     int rc = mqttNetwork.connect(hostname, port);
     if (rc != 0)
         printf("rc from TCP connect is %d\r\n", rc);
- // Send a message with QoS 0
+
+    // Connect the MQTT Client
+    MQTTPacket_connectData data = MQTTPacket_connectData_initializer;
+    data.MQTTVersion = 3;
+    data.clientID.cstring = "92f03640-f570-4f80-b618-1b0dd4513f41"; //Généré sur internet
+    data.username.cstring = "LeCoqGalmotSeghir";
+    data.password.cstring = "project-iot";
+    if ((rc = client.connect(data)) != 0)
+        printf("rc from MQTT connect is %d\r\n", rc);
+
+    // Subscribe to the same topic we will publish in
+    if ((rc = client.subscribe(topic1, MQTT::QOS2, messageArrived)) != 0)
+        printf("rc from MQTT subscribe is %d\r\n", rc);
+
+    // Send a message with QoS 0
     MQTT::Message message;
 
     // QoS 0
     char buf[100];
-    sprintf(buf, "Soulei - Température : \r\n");
+    sprintf(buf, "Hello World!  QoS 0 message from 6TRON\r\n");
 
     message.qos = MQTT::QOS0;
     message.retained = false;
@@ -71,7 +86,7 @@ int main() {
     message.payload = (void*)buf;
     message.payloadlen = strlen(buf)+1;
 
-    rc = client.publish(topic, message);
+    rc = client.publish(topic1, message);
 
     // yield function is used to refresh the connexion
     // Here we yield until we receive the message we sent
@@ -85,36 +100,4 @@ int main() {
     // Bring down the 6LowPAN interface
     net->disconnect();
     printf("Done\n");
-}
-
-
-
-
-
-
-//Récupérer la température
-#include "mbed.h"
-
-namespace {
-#define PERIOD_MS 500
-}
-
-static DigitalOut led1(LED1);
-I2C i2c(I2C1_SDA, I2C1_SCL);
-uint8_t lm75_adress = 0x48 << 1;
-
-// main() runs in its own thread in the OS
-// (note the calls to Thread::wait below for delays)
-int main()
-{
-    while (true) {
-        char cmd[2];
-        cmd[0] = 0x00; // adresse registre temperature
-        i2c.write(lm75_adress, cmd, 1);
-        i2c.read(lm75_adress, cmd, 2);
-
-        float temperature = ((cmd[0] << 8 | cmd[1] ) >> 7) * 0.5;
-        printf("Temperature : %f\n", temperature);
-        ThisThread::sleep_for(PERIOD_MS);
-    }
 }
